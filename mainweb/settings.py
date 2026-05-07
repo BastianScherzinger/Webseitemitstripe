bbf2a10a6e3c6f527ae0183165886455718f2793
BastianScherzinger69 <60239763+BastianScherzinger69@users.noreply.github.com>
2026-05-08 01:41:00 +0200

    fix 2

--- mainweb/settings.py
+++ mainweb/settings.py
@@ -28,8 +28,13 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-CHANGE_THIS_IN_PRODUCTION'
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 # Erlaubte Hosts - '*' erlaubt alles auf Railway
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    'luviq.de',
+    'www.luviq.de',
+    'luviq-luisa-production.up.railway.app', # Deine normale Railway-URL
+    'localhost',
+    '127.0.0.1'
+]
 # CSRF-Sicherheit für Railway & PayPal
 CSRF_TRUSTED_ORIGINS = [
     'https://*.up.railway.app',
